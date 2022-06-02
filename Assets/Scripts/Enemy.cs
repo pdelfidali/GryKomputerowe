0959ec170f8ccc10cd9f0aa836aa804a0a6b5aed
pdelfidali <piotrusdf@gmail.com>
2022-06-02 14:01:02 +0200

    Shooting enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,14 +9,24 @@ public class Enemy : MonoBehaviour{
     public PlayerShooting player;
     public float moveSpeed = 5f;
     public float damage = 1f;
+    public bool canMove = true;
+    public Rigidbody2D rb;
+    public Rigidbody2D playerRB;
 
     private void Start(){
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerShooting>();
+        playerRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate(){
-        transform.position = Vector2.MoveTowards(transform.position, 
-            player.transform.position, moveSpeed * Time.deltaTime);
+        if(canMove){
+            transform.position = Vector2.MoveTowards(transform.position,
+                player.transform.position, moveSpeed * Time.deltaTime);
+        }
+
+        Vector2 lookDir = playerRB.position - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;    
     }
 
     private void OnCollisionEnter2D(Collision2D other){
