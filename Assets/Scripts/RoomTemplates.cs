cc9790cc004820546811fcb4ba0458373cddfc83
Mateusz <maswi0118@gmail.com>
2022-06-10 00:26:11 +0200

    levels design

--- Assets/Scripts/RoomTemplates.cs
+++ Assets/Scripts/RoomTemplates.cs
@@ -47,7 +47,7 @@ public class RoomTemplates : MonoBehaviour{
     }
 
     private void Update(){
-        if (waitTime <= 0 && level <= 4){
+        if (waitTime <= 0 && level <= 14){
             if (levelsWithBoss.Contains(level)){
                 Debug.Log(level);
                 GameObject lastTop = LastTop(levelList.list[level].list);
