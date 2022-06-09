57d0cfb1fb16dd979f9daf259adf815d54610abe
pdelfidali <piotrusdf@gmail.com>
2022-06-09 20:18:17 +0200

    music and bullet fix

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -19,6 +19,7 @@ public class Bullet : MonoBehaviour{
     private void OnCollisionEnter2D(Collision2D other){
         if (!other.gameObject.CompareTag("Player")){
             Destroy(gameObject);
+            
         }
     }
 
